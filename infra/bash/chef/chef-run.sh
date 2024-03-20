b5fe5d08da54d4df278a8081bab5738c401d04f5
Julian Bailey <julian.bailey@takealot.com>
2024-03-20 20:23:52 +0200

    Chef files modified

--- infra/bash/chef/chef-run.sh
+++ infra/bash/chef/chef-run.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-file=clients-2004.txt
+file=clients-2204.txt
 
 while read -r line;
 do
 #knife bootstrap $line -N $line -U julian.bailey --sudo -y -r 'role[HQ],recipe[agent-cinc]'
+#knife node $line delete -y && knife client $line delete -y
 #knife node run_list add  $line 'role[HQ]'
-#knife node run_list remove  $line 'recipe[agent-cinc]'
+#knife node run_list add  $line 'recipe[agent-cinc]'
 #knife node run_list remove $line 'recipe[agent-wazuh]'
-knife node run_list add  $line 'recipe[agent-wazuh-2004]'
-#knife node run_list add $line 'recipe[agent-velociraptor]' -y
+#knife node run_list remove  $line 'recipe[agent-suricata-logrotate]'
+#knife node run_list add $line 'recipe[agent-cinc]' -y
+#knife node show $line -r 
 #knife ssh $line "hostname || ls -l /home/finlay.tachiona"
-#knife ssh -t5 $line "sudo chef-client"
+knife ssh -t5 $line "sudo systemctl stop chef-client && sudo systemctl disable chef-client"
+#knife ssh -t5 $line "sudo cinc-client"
+#knife ssh -t5 $line 'sudo ls -l /etc/sudoers.d/ | grep svc_linux*' 
 #knife ssh -t5 $line "lsb_release -d"
 #knife ssh -t5 $line "sudo systemctl status {auditd,glpi-agent,datacollector,suricata,sysmon,velociraptor_client,wazuh-agent}" &
 #knife ssh -t5 $line "dpkg -l restic" & 
